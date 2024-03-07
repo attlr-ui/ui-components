cac755741f3fd001fb861acc47b1379204e3a955
Axole Maranjana <axolemaranjana4@gmail.com>
2024-03-07 18:06:41 +0200

    Add AAvatar component and dependencies

--- index.tsx
+++ index.tsx
@@ -11,3 +11,11 @@ export { AContainer, type AContainerProps } from './src/acontainer'
 export { ABadge, ABadgeText, type ABadgeProps } from './src/abadge'
 export { AText, type ATextProps } from './src/atext'
 export { AHeading, type AHeadingProps } from './src/aheading'
+export {
+  AAvatar,
+  AAvatarFallbackText,
+  AAvatarImage,
+  AAvatarBadge,
+  type AAvatarProps,
+  AAvatarBadgeProps,
+} from './src/aavatar'
